cac98faeb6a7c175762819ba1b3e53004e8dfe49
asif4762 <zasif855@gmail.com>
2024-05-14 20:36:55 +0600

    starting delete

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ async function run() {
         
     })
 
+    app.delete()
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
